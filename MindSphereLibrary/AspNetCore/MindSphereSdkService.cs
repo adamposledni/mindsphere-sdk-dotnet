7a32f92fc45d845ed0a305c1455fd8247f2f181a
Adam Hrouda <hroudaadam@gmail.com>
2021-03-26 13:12:05 +0100

    asp.net core integration

---
+++ MindSphereLibrary/AspNetCore/MindSphereSdkService.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using MindSphereLibrary.Asset;
+using MindSphereLibrary.Common;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace MindSphereLibrary.AspNetCore
+{
+    public interface IMindSphereSdkService
+    {
+        AssetClient GetAssetClient();
+    }
+
+    public class MindSphereSdkService : IMindSphereSdkService
+    {
+        private HttpClient _httpClient;
+
+        private AssetClient _assetClient;
+
+        private ICredentials _credentials;
+
+        public MindSphereSdkService(HttpClient httpClient, IOptions<MindSphereSdkServiceOptions> options)
+        {
+            _httpClient = httpClient;
+            _credentials = options.Value.Credentials;
+        }
+
+        public AssetClient GetAssetClient()
+        {
+            if (_assetClient == null)
+            {
+                _assetClient = new AssetClient(_credentials, _httpClient);
+            }
+
+            return _assetClient;
+        }
+    }
+
+    public static class MindSphereSdkServiceCollectionExtensions
+    {
+        public static IServiceCollection AddMindSphereSdkService(this IServiceCollection collection,
+            Action<MindSphereSdkServiceOptions> setupAction)
+        {
+            if (collection == null) throw new ArgumentNullException(nameof(collection));
+            if (setupAction == null) throw new ArgumentNullException(nameof(setupAction));
+
+            collection.Configure(setupAction);
+            return collection.AddScoped<IMindSphereSdkService, MindSphereSdkService>();
+        }
+    }
+
+    public class MindSphereSdkServiceOptions
+    {   
+        public ICredentials Credentials { get; set; }
+    }
+
+}
