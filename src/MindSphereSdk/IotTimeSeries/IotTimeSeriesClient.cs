5509251e5d59374688aa5929d9668587f81b26ee
Adam Hrouda <hroudaadam@gmail.com>
2021-03-26 17:03:34 +0100

    create iot client

---
+++ src/MindSphereSdk/IotTimeSeries/IotTimeSeriesClient.cs
@@ -0,0 +1,32 @@
+﻿using MindSphereSdk.Common;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MindSphereSdk.IotTimeSeries
+{
+    /// <summary>
+    /// Client to create, read, update, and delete time series data
+    /// </summary>
+    public class IotTimeSeriesClient : SdkClient
+    {
+        private readonly string _baseUri = "/api/iottimeseries/v3";
+
+        public IotTimeSeriesClient(ICredentials credentials, HttpClient httpClient) : base (credentials, httpClient)
+        {
+
+        }
+
+        public async Task GetTimeSeriesAsync()
+        {
+
+        }
+        public async Task PutTimeSeriesAsync()
+        {
+
+        }
+
+    }
+}
