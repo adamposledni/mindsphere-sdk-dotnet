87c305d68382285e37cc7b45895c7e94e6c7c357
Adam Hrouda <hroudaadam@gmail.com>
2021-05-23 21:06:01 +0200

    restructure

--- usage/ConsoleApp/Program.cs
+++ usage/ConsoleApp/Program.cs
@@ -4,10 +4,6 @@ using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
-using MindSphereSdk.Core.AssetManagement;
-using MindSphereSdk.Core.Common;
-using MindSphereSdk.Core.IotTimeSeries;
-using MindSphereSdk.Core.IotTsAggregates;
 
 namespace ConsoleApp
 {
@@ -15,21 +11,21 @@ namespace ConsoleApp
     {
         static async Task Main(string[] args)
         {
-            AppCredentials appCredentials = AppCredentials.FromJsonFile(@"..\..\..\..\..\mdspcreds.json");
-            HttpClient httpClient = new HttpClient();
+            //AppCredentials appCredentials = AppCredentials.FromJsonFile(@"..\..\..\..\..\mdspcreds.json");
+            //HttpClient httpClient = new HttpClient();
 
-            AssetManagementClient assetClient = new AssetManagementClient(appCredentials, httpClient);
+            //AssetManagementClient assetClient = new AssetManagementClient(appCredentials, httpClient);
             
 
-            ListAssetsRequest request = new ListAssetsRequest()
-            {
-                Size = 200
-            };
-            List<Asset> test = (await assetClient.ListAssetsAsync(request)).ToList();
-            foreach (var item in test)
-            {
-                Console.WriteLine(item.AssetId);
-            }
+            //ListAssetsRequest request = new ListAssetsRequest()
+            //{
+            //    Size = 200
+            //};
+            //List<Asset> test = (await assetClient.ListAssetsAsync(request)).ToList();
+            //foreach (var item in test)
+            //{
+            //    Console.WriteLine(item.AssetId);
+            //}
             
             Console.ReadKey();
         }
