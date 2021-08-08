16bf1d7b78ff72df77ac210e18e9aa92ce738b62
Adam Hrouda <hroudaadam@gmail.com>
2021-08-08 17:32:54 +0200

    create unit tests

---
+++ test/MindSphereSdk.Core.Test/SdkClientTest.cs
@@ -0,0 +1,22 @@
+﻿using MindSphereSdk.Core.AssetManagement;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using Xunit;
+
+namespace MindSphereSdk.Core.Test
+{
+    public class SdkClientTest
+    {
+        [Fact]
+        public void ConstructWithNullCredentials()
+        {
+            // Act
+            Func<AssetManagementClient> act = () => new AssetManagementClient(null, new HttpClient());
+
+            // Assert
+            Assert.Throws<ArgumentNullException>(act);
+        }
+    }
+}
